d9040af56ce6f623f104dd8650158901447f1dac
Sam Albers <sam.albers@gov.bc.ca>
2017-07-06 14:46:27 -0700

    Adding required license files and headers

--- R/check_stn_gap.R
+++ R/check_stn_gap.R
@@ -1,3 +1,16 @@
+# Copyright 2017 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+
 #' @title Check for interval threshold value
 #'
 #' @description Check all realtime hydrometric stations in the Federal-Provincial network that exceed a user supplied time gap in records (defaults to the 60 minutes).
@@ -9,6 +22,7 @@
 #' @export
 #'
 #' @examples
+#' library(HYDATaddons)
 #' check_stn_gap(stations = "07EC003")
 #'
 #' check_stn_gap(stations = "07EC003", gap_thres = 300)
