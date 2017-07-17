8e1a53d692de53cbc1c415663c5bb1bbb7dbf064
Sam Albers <sam.albers@gov.bc.ca>
2017-07-17 16:41:55 -0700

    Rename to hydrolook

---
+++ R/hydrolook.R
@@ -0,0 +1,32 @@
+# Copyright 2017 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+
+
+#' @title Hydrometric Network Diagnostic
+#'
+#' @description This package provides functions and reports to diagnose issues with the Water Survey of Canada Hydrometric network
+#'
+#' \code{hydrolook} package
+#'
+#' @docType package
+#' @name hydrolook
+#'
+#' @importFrom tibble tibble
+#' @importFrom readr read_csv
+#' @importFrom readr cols
+#' @importFrom rmarkdown render
+#' @import tidyhydat
+#' @import dplyr
+#'
+NULL
+
