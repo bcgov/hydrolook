842cfb99432cf47053275b117e4f18014bcedf16
Sam Albers <sam.albers@gov.bc.ca>
2018-04-04 14:19:43 -0700

    added some tests

---
+++ tests/testthat/test-check-package-installation.R
@@ -0,0 +1,10 @@
+context("Test function that checks if packages are installed")
+
+test_that("check_package_installation returns a message when given missing packages",{
+  expect_message(hydrolook:::check_package_installation(c("dplyr2", "ggplot3")))
+})
+
+
+test_that("check_package_installation is silent when given valid packages",{
+  expect_silent(hydrolook:::check_package_installation(c("utils", "stats")))
+})
