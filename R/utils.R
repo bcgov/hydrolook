ac1e09e134a64dc74b531ab0a16cb355f41fc23b
Sam Albers <sam.albers@gmail.com>
2018-05-03 09:36:20 -0700

    message then stop; don't call function

--- R/utils.R
+++ R/utils.R
@@ -24,16 +24,16 @@ check_package_installation <- function(libs_needed) {
 
   ## Output a message
   if(any(pkgs %in% "ggplot2") == TRUE && any(getNamespaceExports("ggplot2") %in% "GeomSf")){
-    stop("Please install the development version of ggplot2 using the devtools package:")
-    stop('install.packages(devtools); devtools::install_github("tidyverse/ggplot2")')
+    message("Please install the development version of ggplot2 using the devtools package:")
+    stop('install.packages(devtools); devtools::install_github("tidyverse/ggplot2")', call. = TRUE)
 
     pkgs <- pkgs[which(!pkgs %in% "ggplot2")]
   }
 
   if(length(pkgs) > 0) {
-    stop("The ",paste0(pkgs, collapse = ", ")," package(s) need to be installed to run this report.")
+    message("The ",paste0(pkgs, collapse = ", ")," package(s) need to be installed to run this report.")
     stop("Paste the following into the console to install the missing packages: install.packages(c(",
-            paste0(sprintf("'%s'", pkgs), collapse = ","),"))")
+            paste0(sprintf("'%s'", pkgs), collapse = ","),"))", call. = TRUE)
   }
 
 }
